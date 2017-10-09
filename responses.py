087da5de7541a6d69207583af92a232443259128
zoox101 <william@thebookers.net>
2017-10-08 20:14:49 -0500

    Updated Lambda Code

--- responses.py
+++ responses.py
@@ -3,7 +3,7 @@ lines = str.split(open('Alexa_Responses.txt').read(), '\n')
 for line in lines:
     if line != '':
         tup = line.split(': ')
-        print 'responses[' + tup[0] + '] = ' + tup[1]
+        print 'responses[\'' + tup[0] + '\'] = \'' + tup[1] + '\''
 
 '''
 responses = {}
