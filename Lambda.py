56224408f72a6fd01c4344c40ff3af43540ce10c
JJ Brown <jack.brown@ou.edu>
2017-10-08 20:49:00 -0500

    Improved code quality

--- Lambda.py
+++ Lambda.py
@@ -79,61 +79,60 @@ def get_directions_for_intent(intent, session):
     room_name = intent['slots']['RoomName']['value']
     speech_output = get_directions_for_room(room_name)
 
-    responses = {}
-    responses['kxou'] = 'Its the glass room in front of you to the left!'
-    responses['post_office'] = 'Its on the right side of the hallway to your left. '
-    responses['sooner_card'] = 'Its on the left side of the hallway to your left.'
-    responses['ou_passport'] = 'Its on the left side of the hallway to your left.'
-    responses['student_art_gallery'] = 'Its on the left side of the hallway to your left. '
-    responses['starbucks'] = 'Its on the left side of the hallway to your left.'
-    responses['credit_union'] = 'Go down the hall to your left. It will be on your left after the ramp.'
-    responses['crossroads'] = 'Go down the hall to your left. It will be on your right after the ramp.'
-    responses['lgbtq_lounge'] = 'Go down the hall to your left. Turn right after going down the ramp. Its on the right side just past Crossroads. '
-    responses['sooner_room'] = 'Go down the hall to your left. Turn right after going down the ramp. Its at the end of the hallway on the left.'
-    responses['student_government_association'] = 'Go down the hall to your left. Turn right after going down the ramp. Its at the end of the hallway on the right.'
-    responses['one_university_store'] = 'Its the glass room to your right!'
-    responses['union_market'] = 'Its on the left side of the hallway to your right.'
-    responses['will_rogers_room'] = 'Its on the left side of the of the hallway to your right.'
-    responses['food_court'] = 'Its on the right side of the hallway to your right.'
-    responses['clarke_anderson_room'] = 'Go down the hallway to your right. It will be on the left just past the room with all the chairs.'
-    responses['stuart_landing'] = 'Turn around and use the stairs to go up to the second floor. Its immediately in front of the staircase.'
-    responses['alma_wilson_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the first room on your left.'
-    responses['pioneer_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the first room on your right.'
-    responses['david_f_schrage_traditions_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the second room on your right.'
-    responses['john_houchin_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the second room on your left.'
-    responses['louise_houchin_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the third room on your left.'
-    responses['david_l_boren_lounge'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the third room on your right.'
-    responses['presidents_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the fourth room on your left.'
-    responses['meacham_auditorium'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. Its on the right side of the atrium at the end of the hall.'
-    responses['volunteer_office'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. Its at the end of the hallway down the short flight of stairs.'
-    responses['student_affairs'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. Go all the way down the hallway and down the short flight of stairs. It will be at the end of the hallway on your right.'
-    responses['conoco_student_leadership_wing'] = 'Turn around and use the stairs to go up to the second floor. Then turn left. Go all the way down the hallway and down the short flight of stairs. It will be on the right side of the hallway on your right.'
-    responses['beaird_lounge'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. It will be the room on your right after the double doors.'
-    responses['flint_study_center_computer_lab'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. Its on your left after the double doors. '
-    responses['crimson_meeting_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. Its on your left after the double doors. '
-    responses['bartlet_study_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. Its on your left after the double doors. '
-    responses['frontier_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the first room on your left after the short flight of stairs. '
-    responses['weitzenhoffer_dining_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the second room on your left after the short flight of stairs. '
-    responses['heritage_room'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the third room on your left after the short flight of stairs. '
-    responses['crawford_university_club'] = 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the big room on your right after the short flight of stairs. '
-    responses['career_services'] = 'Turn around and use the stairs to go up to the third floor. It will be the room on your left.'
-    responses['molly_shi_boren_ballroom'] = 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the room on your right. '
-    responses['governors_room'] = 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the first room on your right after the ballroom. '
-    responses['regents_room'] = 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the second room on your right after the ballroom. '
-    responses['associates_room'] = 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the third room on your right after the ballroom. '
-    responses['scholars_room'] = 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the room in the corner on your left after the ballroom. '
-    responses['meacham_balcony'] = 'Go down the hallway to your left then turn right after the ramp. Use the stairs on your left to get to the third floor. It will be at the end of the hallway to your right after a short flight of stairs. '
-    responses['student_life'] = 'Go down the hallway to your left then turn right after the ramp. Use the stairs on your left to get to the third floor. It will be on your left.'
-    responses['student_leadership_wing'] = 'Go down the hallway to your left then turn right after the ramp. Use the stairs on your left to get to the third floor. Go through the student life hallway on your left. '
-    responses['union_administration_and_programming_board'] = 'Turn around and use the stairs to go up to the fourth floor. Then turn left. It will be the first room on your right.'
-    responses['alumni_association'] = 'Turn around and use the stairs to go up to the fourth floor. Then turn left. It will be the first room on your left.'
-    responses['paul_massad_conference_room'] = 'Turn around and use the stairs to go up to the fourth floor. It will be the room in front of you to the right. '
+    responses = {'kxou': 'Its the glass room in front of you to the left!',
+                 'post_office': 'Its on the right side of the hallway to your left. ',
+                 'sooner_card': 'Its on the left side of the hallway to your left.',
+                 'ou_passport': 'Its on the left side of the hallway to your left.',
+                 'student_art_gallery': 'Its on the left side of the hallway to your left. ',
+                 'starbucks': 'Its on the left side of the hallway to your left.',
+                 'credit_union': 'Go down the hall to your left. It will be on your left after the ramp.',
+                 'crossroads': 'Go down the hall to your left. It will be on your right after the ramp.',
+                 'lgbtq_lounge': 'Go down the hall to your left. Turn right after going down the ramp. Its on the right side just past Crossroads. ',
+                 'sooner_room': 'Go down the hall to your left. Turn right after going down the ramp. Its at the end of the hallway on the left.',
+                 'student_government_association': 'Go down the hall to your left. Turn right after going down the ramp. Its at the end of the hallway on the right.',
+                 'one_university_store': 'Its the glass room to your right!',
+                 'union_market': 'Its on the left side of the hallway to your right.',
+                 'will_rogers_room': 'Its on the left side of the of the hallway to your right.',
+                 'food_court': 'Its on the right side of the hallway to your right.',
+                 'clarke_anderson_room': 'Go down the hallway to your right. It will be on the left just past the room with all the chairs.',
+                 'stuart_landing': 'Turn around and use the stairs to go up to the second floor. Its immediately in front of the staircase.',
+                 'alma_wilson_room': 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the first room on your left.',
+                 'pioneer_room': 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the first room on your right.',
+                 'david_f_schrage_traditions_room': 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the second room on your right.',
+                 'john_houchin_room': 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the second room on your left.',
+                 'louise_houchin_room': 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the third room on your left.',
+                 'david_l_boren_lounge': 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the third room on your right.',
+                 'presidents_room': 'Turn around and use the stairs to go up to the second floor. Then turn left. It will be the fourth room on your left.',
+                 'meacham_auditorium': 'Turn around and use the stairs to go up to the second floor. Then turn left. Its on the right side of the atrium at the end of the hall.',
+                 'volunteer_office': 'Turn around and use the stairs to go up to the second floor. Then turn left. Its at the end of the hallway down the short flight of stairs.',
+                 'student_affairs': 'Turn around and use the stairs to go up to the second floor. Then turn left. Go all the way down the hallway and down the short flight of stairs. It will be at the end of the hallway on your right.',
+                 'conoco_student_leadership_wing': 'Turn around and use the stairs to go up to the second floor. Then turn left. Go all the way down the hallway and down the short flight of stairs. It will be on the right side of the hallway on your right.',
+                 'beaird_lounge': 'Turn around and use the stairs to go up to the second floor. Then turn right. It will be the room on your right after the double doors.',
+                 'flint_study_center_computer_lab': 'Turn around and use the stairs to go up to the second floor. Then turn right. Its on your left after the double doors. ',
+                 'crimson_meeting_room': 'Turn around and use the stairs to go up to the second floor. Then turn right. Its on your left after the double doors. ',
+                 'bartlet_study_room': 'Turn around and use the stairs to go up to the second floor. Then turn right. Its on your left after the double doors. ',
+                 'frontier_room': 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the first room on your left after the short flight of stairs. ',
+                 'weitzenhoffer_dining_room': 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the second room on your left after the short flight of stairs. ',
+                 'heritage_room': 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the third room on your left after the short flight of stairs. ',
+                 'crawford_university_club': 'Turn around and use the stairs to go up to the second floor. Then turn right. Its the big room on your right after the short flight of stairs. ',
+                 'career_services': 'Turn around and use the stairs to go up to the third floor. It will be the room on your left.',
+                 'molly_shi_boren_ballroom': 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the room on your right. ',
+                 'governors_room': 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the first room on your right after the ballroom. ',
+                 'regents_room': 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the second room on your right after the ballroom. ',
+                 'associates_room': 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the third room on your right after the ballroom. ',
+                 'scholars_room': 'Turn around and use the stairs to go up to the third floor. Then turn right. It will be the room in the corner on your left after the ballroom. ',
+                 'meacham_balcony': 'Go down the hallway to your left then turn right after the ramp. Use the stairs on your left to get to the third floor. It will be at the end of the hallway to your right after a short flight of stairs. ',
+                 'student_life': 'Go down the hallway to your left then turn right after the ramp. Use the stairs on your left to get to the third floor. It will be on your left.',
+                 'student_leadership_wing': 'Go down the hallway to your left then turn right after the ramp. Use the stairs on your left to get to the third floor. Go through the student life hallway on your left. ',
+                 'union_administration_and_programming_board': 'Turn around and use the stairs to go up to the fourth floor. Then turn left. It will be the first room on your right.',
+                 'alumni_association': 'Turn around and use the stairs to go up to the fourth floor. Then turn left. It will be the first room on your left.',
+                 'paul_massad_conference_room': 'Turn around and use the stairs to go up to the fourth floor. It will be the room in front of you to the right. '}
 
     # establish control settings for the response object
     card_title = intent['name']
     key = intent['slots']['RoomName']['value']
 
-    key = key.replace(' ', '_');
+    key = key.replace(' ', '_')
     key = key.lower()
 
     speech_output = responses[key]
