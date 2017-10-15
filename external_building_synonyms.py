bb637cb3074a347c50e0cd7e36b36526f76d7e10
JJ Brown <jack.brown@ou.edu>
2017-10-15 14:18:18 -0500

    Addd synonyms to building directions, fixed typos

--- external_building_synonyms.py
+++ external_building_synonyms.py
@@ -1,47 +1,47 @@
 external_building_synonyms = {
-    "adams_center": [""],
+    "adams_center": [], # the parser can match to the key with no synonyms defined
 
-    "adams_hall": [""],
+    "adams_hall": [],
 
-	"administration_building": [""],  
+	"administration_building": ["admin", "administration", "admin building"],
 
-	"andrew_m_coats_hall": [""],
+	"andrew_m_coats_hall": ["coats hall"],
 	
 	"armory": ["army r. o. t. c.", "navy r. o. t. c."],
 	
-	"asp_avenue_parking_facility": [""],
+	"asp_avenue_parking_facility": ["asp parking garage", "union parking garage"],
 	
-	"bizzell_memorial_library": [""],
+	"bizzell_memorial_library": ["bizzell", "library"],
 	
-	"george_lynn_cross_hall": [""],
+	"george_lynn_cross_hall": ["george lynn cross"],
 	
 	"boyd_house": ["President's House"],
 	
-	"buchanan_hall": [""],
+	"buchanan_hall": ["buchanan", "bursar", "bursar's office"],
 	
-	"burton_hall": [""],
+	"burton_hall": [],
 	
-	"carnegie_building": [""],
+	"carnegie_building": ["carnegie hall"],
 	
-	"carpenter_hall": [""],
+	"carpenter_hall": ["carpenter"],
 	
-	"carson_engineering_center": [""],
+	"carson_engineering_center": ["carson"],
 	
-	"cate_center": [""],
+	"cate_center": ["cate"],
 	
-	"catlett_music_center": [""],
+	"catlett_music_center": ["catlett", "music building", "orchestra"],
 	
-	"chemistry_building": [""],
+	"chemistry_building": ["old chemistry building"],
 	
-	"collings_hall": [""],
+	"collings_hall": ["collings"],
 	
 	"collums_commissary": ["collums building"],
 	
-	"commons_restaurant": [""],
+	"commons_restaurant": ["commons"],
 	
 	"copeland_hall": ["journalism building"],
 	
-	"couch_cafeteria": [""],
+	"couch_cafeteria": ["the caf", "the calf", "couch restaurants"],
   
   #Add rest here
 }
