62fccd62714a933aea2248250f1d3ae0297b485e
Emily Black <emilycblack95@gmail.com>
2017-10-15 13:53:11 -0500

    1/5 of the way completed

---
+++ external_room_directions.py
@@ -0,0 +1,106 @@
+external_building_directions = {'adams_center': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south, even past Lindsey street, until you come across three X shaped buildings. Adams Center is on the first floor of the right most building you'll see.',
+	'adams_hall': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south, even past Lindsey street, until you come across three X shaped buildings. Adams Hall is located on the second floor and above in the right most building.',
+	'administration_building': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south until you reach Lindsey street. When you hit Lindsey Street, turn left and walk directly due east until you hit Asp avenue. Then turn right and walk directly due south on Asp avenue until you pass Fourth street. The administration building will be on your right.',
+	'andrew_m_coats_hall': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south until you reach Lindsey street. When you hit Lindsey Street, turn left and walk directly due east until you hit Asp avenue. Then turn right and walk directly due south on Asp avenue until you reach Timberdell road. Turn right on Timberdell road and walk directly due west until you can take your first left. The Andrew M Coats hall should be located directly down that road.',
+	'armory':  'Walk past Crossroads and out the southern exit. Make your way to the south oval. Once you hit the south oval, turn left and walk directly due east towards the stadium. The armory will be on your right, just before you hit the stadium.',
+	'asp_avenue_parking_facility': 'Walk past Crossroads and out the southern exit. Make your way to the south oval. Once you hit the south oval, turn left and walk directly due east towards the stadium. Before walking past the armory, turn right and walk directly due south until you hit Asp avenue. Keep walking directly south and on your left should be the Asp avenue parking facility.',
+	'bizzell_memorial_library': 'Walk past Crossroads and out the southern exit. Make your way to the south oval. Once you hit the south oval, turn right and walk directly due west until you can make your first right. The Bizzell library is located right in front of the south oval.',
+	'george_lynn_cross_hall': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south until you can make a left on the second building down.',
+	'boyd_house': 'Walk past the food court and out the western exit. Make your way to the north oval and walk directly due north until you hit Boyd street. On the left side, across the street, is the Boyd House.',
+	'buchanan_hall': 'Walk past Crossroads and out the southern exit. The building directly due south of the Union is Buchanan Hall.',
+	'burton_hall': 'Walk past Crossroads and out the southern exit. Turn right and walk directly due west until you hit Elm avenue. Cross the street, turn right and walk directly due north. Burton should be the first building on your left.',
+	'carnegie_building': 'Walk past Crossroads and out the southern exit. Turn right and walk directly due west until you turn right and walk towards the north oval. Carnegie will be the first building on your right.',
+	'carpenter_hall': 'Walk towards the One University store. Before hitting it, turn right, and make your way out the eastern exit. Turn left and walk directly due north. You'll pass the union and the union parking garage. The next building after that will be Carpenter Hall.',
+	'carson_engineering_center': 'Walk towards the One University store. Before hitting it, turn right, and make your way out the eastern exit. Turn left and walk directly due north, until you hit Boyd street. Once you hit Boyd street, cross Asp avenue and the Carson Engineering Center should be directly in front of you.',
+	'cate_center': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south, even past Lindsey street. The Cate Center should be between the first set of buildings you hit after crossing Lindsey, and between the street you cross to get to the three dorm towers.',
+	'catlett_music_center': 'Walk past Crossroads and out the southern exit. Turn right and walk directly due west until you hit Elm avenue. Cross the street, turn right and walk directly due north. The Catlett Music Center will be the last building before you hit Boyd street.',
+	'chemistry_building': 'Walk past Crossroads and out the southern exit. Turn right and walk directly due west until you turn right and walk towards the north oval. The Chemistry building should be the second building on your left.',
+	'collings_hall': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south. Collings Hall will be the fourth building on your right.',
+	'collums_commissary': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south until you reach Lindsey street. When you hit Lindsey Street, turn left and walk directly due east. Once you cross Jenkins avenue, the Collums Commissary will be the second building on your left.',
+	'commons_restaurant': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south, even past Lindsey street. Past the three dorms, you'll eventually run into a 3 pointed star shaped building. The Commons Restaurant, also known as the Jim Thorpe Multi-Cultural Center, will be located on the north west side of the block.',
+	'copeland_hall': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south. Copeland Hall will be the fifth building on your right.',
+	'couch_cafeteria': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south, even past Lindsey street. Past the three dorms, you'll run into a circular shaped building. That will be the Couch Cafeteria.',
+	# Have to finish the following below this.
+  # Couch  Center 
+	# Cross Center 
+	# Dale Hall 
+	# Dale Hall Tower 
+	# Devon Energy Hall 
+	# Ellison Hall 
+	# Elm Ave Parking Facility 
+	# Engineering Laboratory 
+	# Evans Hall, Administration Bldg. 
+	# ExxonMobil Lawrence G. Rawl
+	# Practice Facility 
+	# Fears Structural Engineering Lab 
+	# Felgar Hall 
+	# Fine Arts Center, Drama 
+	# Forum Building (OCCE) 
+	# Four Partners Place 
+	# Fred Jones Jr. Memorial Art Center 
+	# Gaylord Hall 
+	# The Gaylord Family 
+	# Oklahoma Memorial Stadium 
+	# Gittinger Hall, English 
+	# Goddard Health Center 
+	# Gould Hall 
+	# Hester Hall 
+	# Housing  Offices 
+	# Huston Huffman Physical Fitness Center 
+	# Jacobs Field, Track 
+	# Jacobson Hall (Visitor Center) 
+	# Jefferson House 
+	# Jimmie Austin OU Golf Course 
+	'jim_thorpe_multi_cultural_center': 'Walk past Crossroads and out the southern exit. Make your way to the south oval and keep walking directly due south, even past Lindsey street. Past the three dorms, you'll eventually run into a 3 pointed star shaped building. The Jim Thorpe Multi-Cultural Center, also known as the Commons Restaurant, will be located on the north west side of the block.', 
+	# Jones House 
+	# Kaufman Hall  
+	# Kraettli Apartments 
+	# Lissa & Cy Wagner Student 
+	# Academic Services Center 
+	# Lloyd Noble Center 
+	# L. Dale Mitchell Baseball Park 
+	# McCarter Hall (OCCE) 
+	# Michael F. Price College of Business 
+	# Monnet Hall 
+	# Mosier Indoor Athletic Facility 
+	# National Weather Center 
+	# Newman Hall 
+	# Nielsen Hall, Physics 
+	# Norman Fire Department #3 
+	# Nuclear Engineering Laboratory 
+	# Observatory  
+	# Oklahoma Memorial Union 
+	# Old Faculty Club
+	# One Partners Place 
+	# OU Police Division (OUPD) 
+	# OU Traditions Square East 
+	# OU Traditions Square West 
+	# Parking Services (Robertson Hall) 
+	# Personnel Services (NEL Building) 
+	# Physical Plant Complex 
+	# Physical Sciences Center 
+	# Reynolds Performing Arts Center 
+	# Rhyne Hall, Social Work 
+	# Richards Hall, Zoology 
+	# Robertson Hall 
+	# Rupel J. Jones Theatre, Drama 
+	# Sam Noble Oklahoma Museum of Natural History 
+	# Sarkey’s Energy Center 
+	# Science Hall 
+	# Sells Swim Complex 
+	# Stephenson Life Sciences Research Center 
+	# Stephenson Research & Technology Center 
+	# Sutton Hall 
+	# T. Howard McCasland Field House 
+	# Three Partners’ Place 
+	# Thurman White Hall (Forum Bldg. OCCE) 
+	# Transportation Operations Center 
+	# Two Partners’ Place 
+	# University Book Exchange (Main Store) 
+	# University of Oklahoma Foundation
+	# Viersen Gymnastics Center 
+	# Wagner Dining Facility 
+	# Walker Tower 
+	# Wilkinson House 
+	# Whitehand Hall 
+	# Anne and Henry Zarrow Hall
