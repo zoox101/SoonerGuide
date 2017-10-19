4b46b4c25020919de8d19fa7158600e0c4588663
JJ Brown <jack.brown@ou.edu>
2017-10-19 10:44:59 -0500

    Improved synonyms

--- external_building_synonyms.py
+++ external_building_synonyms.py
@@ -1,5 +1,5 @@
 external_building_synonyms = {
-    "adams_center": [],
+    "adams_center": ["adams tower"],
 
     "adams_hall": ["adams"],
 
@@ -33,7 +33,7 @@ external_building_synonyms = {
 
     "carson_engineering_center": ["carson"],
 
-    "cate_center": ["cate"],
+    "cate_center": ["cate", "cate 1", "cate 2", "cate 3", "cate 4", "cate 5", "cate cafeteria", "cate food court", "cate lounge"],
 
     "catlett_music_center": ["catlett", "music building", "orchestra"],
 
@@ -47,7 +47,7 @@ external_building_synonyms = {
 
     "couch_cafeteria": ["caf", "calf", "couch restaurants"],
 
-    "couch_center": [],
+    "couch_center": ["couch tower"],
 
     "cross_center": [],
 
@@ -55,11 +55,11 @@ external_building_synonyms = {
 
     "dale_hall_tower": ["dale tower"],
 
-    "devon_energy_hall": ["devon", "devon hall", "engineering"],
+    "devon_energy_hall": ["devon", "devon hall", "engineering", "computer science building", "school of computer science"],
 
     "dunham_residential_college": ["dunham", "dunham college"],
 
-    "ellison_hall": [],
+    "ellison_hall": ["ellison"],
 
     "elm_ave_parking_facility": ["elm parking garage"],
 
@@ -80,15 +80,15 @@ external_building_synonyms = {
 
     "fine_arts_center": ["drama building"],
 
-    "five_partners_place": [],
+    "five_partners_place": ["five partner place"],
 
-    "forum_building": [],
+    "forum_building": ["forum"],
 
-    "four_partners_place": [],
+    "four_partners_place": ["four partner place"],
 
     "fred_jones_junior_memorial_art_center": ["art museum", "fred jones"],
 
-    "gaylord_hall": ["broadcasting building", "gaylord"],
+    "gaylord_hall": ["broadcasting building", "gaylord", "t. v. studio"],
 
 # Took out 'the' because the search algorithm already accounts for a 'the' at the front of any name
     "gaylord_family_oklahoma_memorial_stadium": ["oklahoma memorial stadium", "gaylord stadium", "football stadium",
@@ -98,7 +98,7 @@ external_building_synonyms = {
 
     "goddard_health_center": ["health center", "goddard"],
 
-    "gould_hall": ["gould"],
+    "gould_hall": ["gould", "gold", "ghoul"],
 
     "headington_hall": ["headington"],
 
@@ -116,93 +116,93 @@ external_building_synonyms = {
 
     "jimmie_austin_o_u_golf_course": ["o. u. golf course", "golf course"],
 
-    "jim_thorpe_multi_cultural_center": [],
+    "jim_thorpe_multi_cultural_center": ["jim thorpe", "multi cultural center", "jim thorpe building", "multi cultural building"],
 
-    "kaufman_hall": [],
+    "kaufman_hall": ["kaufman", "cough men"],
 
     "kraettli_apartments": [],
 
-    "lissa_and_cy_wagner_student_academic_services_center": ["wagner student center", "wagner center"],
+    "lissa_and_cy_wagner_student_academic_services_center": ["wagner student center", "wagner center", "student academic services", "academic services"],
 
-    "lloyd_noble_center": ["lloyd noble"],
+    "lloyd_noble_center": ["lloyd noble", "graduation"],
 
-    "l_dale_mitchell_baseball_park": ["baseball park"],
+    "l_dale_mitchell_baseball_park": ["baseball park", "baseball stadium", "baseball field", "baseball diamond", "baseball"],
 
-    "mccarter_hall": [],
+    "mccarter_hall": ["mccarter"],
 
-    "michael_f_price_college_of_business": ["price college of business", "college of business"],
+    "michael_f_price_college_of_business": ["price college of business", "college of business", "business college", "price"],
 
-    "monnet_hall": [],
+    "monnet_hall": ["monnet"],
 
     "mosier_indoor_athletic_facility": ["indoor athletic facility", "indoor track"],
 
-    "national_weather_center": ["weather center"],
+    "national_weather_center": ["weather center", "weather building"],
 
-    "newman_hall": [],
+    "newman_hall": ["newman"],
 
-    "nielsen_hall": [],
+    "nielsen_hall": ["physics building", "nielsen", "kneel sun"],
 
     "norman_fire_department_number_3": ["fire department number three"],
 
-    "nuclear_engineering_laboratory": ["nuclear lab"],
+    "nuclear_engineering_laboratory": ["nuclear lab", "nuclear laboratory"],
 
-    "observatory": ["telescope"],
+    "observatory": ["telescope", "old observatory"],
 
-    "old_faculty_club": [],
+    "old_faculty_club": ["old faculty building"],
 
-    "one_partners_place": ["south campus"],
+    "one_partners_place": ["south campus", "one partner place"],
 
     "o_u_police_department": ["o. u. p. d.", "police department"],
 
-    "o_u_traditions_square_east": ["traditions east"],
+    "o_u_traditions_square_east": ["traditions east", "traditions"],
 
     "o_u_traditions_square_west": ["traditions west"],
 
     "parking_services": ["parking passes"],
 
-    "physical_sciences_center": ["physical science building"],
+    "physical_sciences_center": ["physical science building", "blender"],
 
     "reynolds_performing_arts_center": ["performing arts center"],
 
-    "richards_hall": ["richards", "zoology"],
+    "richards_hall": ["richards", "zoology", "zoology building"],
 
     "robertson_hall": ["robertson"],
 
-    "rupel_j_jones_theatre": ["drama theatre", "rupel jones", "jones"],
+    "rupel_j_jones_theatre": ["drama theatre", "rupel jones", "jones", "theatre", "theater"],
 
     "sam_noble_oklahoma_museum_of_natural_history": ["sam noble museum", "sam noble", "museum of natural history"],
 
     "sarkeys_energy_center": ["sarkeys", "energy center"],
 
-    "sarkeys_physical_fitness_center": ["huffman", "gym", "physical fitness center", "o. u. gym"],
+    "sarkeys_physical_fitness_center": ["huffman", "gym", "physical fitness center", "o. u. gym", "huff man"],
 
-    "science_hall": ["science building"],
+    "science_hall": ["science building", "old science building"],
 
     "sells_swim_complex": ["sells complex", "swim complex", "o. u. pool", "pool"],
 
-    "sooner_cottage_suites": ["sooner suites", "cottage suites"],
+    "sooner_cottage_suites": ["sooner suites", "cottage suites", "sooner cottages"],
 
     "stephenson_life_sciences_research_center": ["life sciences", "life sciences research center"],
 
-    "stephenson_research_and_technology_center": ["research and technology center", "stephenson"],
+    "stephenson_research_and_technology_center": ["research and technology center", "stephenson", "stepheson hall", "stepheson center", "technology center"],
 
     "sutton_hall": ["sutton"],
 
     "t_howard_mccasland_field_house": ["mccasland house", "field house"],
 
-    "theta_m_dempsey_transportation_operations_center": ["dempsey center", "transporation operations center"],
+    "theta_m_dempsey_transportation_operations_center": ["dempsey center", "transporation operations center", "transportation center", "transportation building"],
 
-    "three_partners_place": ["innovation hub"],
+    "three_partners_place": ["innovation hub", "three partner place"],
 
-    "two_partners_place": [],
+    "two_partners_place": ["two partner place"],
 
     "university_book_exchange": ["book store", "text book store", "book exchange"],
 
     "university_of_oklahoma_foundation": ["oklahoma foundation"],
 
-    "viersen_gymnastics_center": ["gymnastics center", "viersen center", "viersen building"],
+    "viersen_gymnastics_center": ["gymnastics center", "viersen center", "viersen building", "gymnastics building", "gymnastics"],
 
-    "walker_tower": ["walker", "walker dorms"],
+    "walker_tower": ["walker", "walker dorms", "walker tower", "walker adams mall", "dorms", "dorm towers"],
 
-    "whitehand_hall": ["whitehand"]
+    "whitehand_hall": ["whitehand", "white hand", "white hand hall"]
 }
